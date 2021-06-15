a019a79fa20c08b91eaaa519a46ddba8160b34fb
Tushar_Maity <68024552+Tushar-Maity@users.noreply.github.com>
2021-06-15 23:32:29 +0530

    ErrorModal dynamic with errorhandler

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -8,7 +8,7 @@ import './ErrorModal.css'
 const ErrorModal = (props) => {
     return (
         <div>
-            <div className="backdrop"/>
+            <div className="backdrop" onClick={props.onConfirm}/>
             <Card className="modal">
                 <header className="header">
                     <h2>{props.title}</h2>
@@ -17,7 +17,7 @@ const ErrorModal = (props) => {
                     <p>{props.message}</p>
                 </div>
                 <footer className="actions">
-                    <Button>Okay</Button>
+                    <Button onClick={props.onConfirm}>Okay</Button>
                 </footer>
             </Card>
         </div>
